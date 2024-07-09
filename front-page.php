654499cbf9b99ac67a8fdc6d455287d0b54da416
Michael <mw910114@gmail.com>
2024-07-09 01:19:42 +0100

    rudimentary markup

--- front-page.php
+++ front-page.php
@@ -20,11 +20,16 @@ get_header();
 
         <h1><?php echo $title; ?></h1>
         <p><?php echo $subtitle; ?></p>
-        <button class="btn"><?php echo $button['title']; ?></button>
+
         <button class="btn btn--solid"><?php echo $button['title']; ?></button>
-        <button class="btn btn--solid-white"><?php echo $button['title']; ?></button>
-        <button class="btn btn--solid-grey"><?php echo $button['title']; ?></button>
-        <button class="btn btn--outline"><?php echo $button['title']; ?></button>
+        <!-- <button class="btn"><?php //echo $button['title']; 
+                                    ?></button>
+        <button class="btn btn--solid-white"><?php //echo $button['title']; 
+                                                ?></button>
+        <button class="btn btn--solid-grey"><?php //echo $button['title']; 
+                                            ?></button>
+        <button class="btn btn--outline"><?php //echo $button['title']; 
+                                            ?></button> -->
 
 
         <!-- echo $button['url']; -->
@@ -35,17 +40,59 @@ get_header();
 
 
     <!-- TESTED AND USED -->
+    <section>
+        <div class="tested">
+            <h2>Tested and used by the best</h2>
+            <div class="">
+                <img src="<?php echo $hero_img['url']; ?>">
+                <img src="<?php echo $hero_img['url']; ?>">
+                <img src="<?php echo $hero_img['url']; ?>">
+                <img src="<?php echo $hero_img['url']; ?>">
+                <img src="<?php echo $hero_img['url']; ?>">
+            </div>
+        </div>
+    </section>
     <!-- END TESTED AND USED -->
 
 
     <!-- STAT CONTAINER -->
+    <section>
+        <div class="">STAT</div>
+    </section>
     <!-- END STAT CONTAINER -->
 
 
     <!-- IMPROVE YOUR GAMEPLAY -->
+    <section>
+        <div data-aos="fade-up" class="title-section">
+            <h6>skybox edge for teams</h6>
+            <h2>IMPROVE YOUR GAMEPLAY</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+    </section>
     <!-- END IMPROVE YOUR GAMEPLAY -->
 
     <!-- ALTERNATING VIDEO COLUMNS -->
+    <section>
+        <!-- row 1 -->
+        <div class="alternating-video-column">
+            <img src="<?php echo $hero_img['url']; ?>">
+            <div data-aos="fade-up" class="title-section">
+                <h6>skybox edge for teams</h6>
+                <h2>Lorum ipsum dolar sit amet</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+        </div>
+        <!-- row 2 -->
+        <div class="alternating-video-column">
+            <img src="<?php echo $hero_img['url']; ?>">
+            <div data-aos="fade-up" class="title-section">
+                <h6>skybox edge for teams</h6>
+                <h2>Lorum ipsum dolar sit amet</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+        </div>
+    </section>
     <!-- END ALTERNATING VIDEO COLUMNS -->
 
 </div>
@@ -66,7 +113,7 @@ get_header();
 
         <div data-aos="fade-up" class="title-section">
             <h6><?php echo get_field('pricing_title')['small_text']; ?></h6>
-            <h2><?php echo get_field('pricing_title')['title']; ?></h2>
+            <h2>start from today</h2>
             <p><?php echo get_field('pricing_title')['description']; ?></p>
         </div>
     </section>
@@ -76,7 +123,9 @@ get_header();
     <section>
         <div data-aos="fade-up" class="title-section">
             <h6><?php echo get_field('testimonials_title')['small_text']; ?></h6>
-            <h2><?php echo get_field('testimonials_title')['title']; ?></h2>
+            <h2>Take their word for it</h2>
+            <!-- <h2><?php //echo get_field('testimonials_title')['title']; 
+                        ?></h2> -->
             <p><?php echo get_field('testimonials_title')['description']; ?></p>
         </div>
     </section>
@@ -85,8 +134,12 @@ get_header();
 
     <!-- CTA -->
     <section>
-
-
+        <div data-aos="fade-up" class="cta">
+            <h6><?php echo get_field('testimonials_title')['small_text']; ?></h6>
+            <h2><?php echo get_field('testimonials_title')['title']; ?></h2>
+            <p><?php echo get_field('testimonials_title')['description']; ?></p>
+        </div>
+        <img src="<?php echo $hero_img['url']; ?>">
     </section>
     <!-- END CTA -->
 </div>
@@ -99,7 +152,9 @@ get_header();
     <section id="faq">
         <div data-aos="fade-up" class="title-section">
             <h6><?php echo get_field('faqs_title')['small_text']; ?></h6>
-            <h2><?php echo get_field('faqs_title')['title']; ?></h2>
+            <h2>Questions</h2>
+            <!-- <h2><?php //echo get_field('faqs_title')['title']; 
+                        ?></h2> -->
             <p><?php echo get_field('faqs_title')['description']; ?></p>
         </div>
     </section>
--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Displays the header content
  *
@@ -11,132 +12,137 @@
 <html <?php language_attributes(); ?>>
 <?php
 $edge_settings = edge_get_theme_options(); ?>
+
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<?php wp_head(); ?>
+	<meta charset="<?php bloginfo('charset'); ?>" />
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+	<?php wp_head(); ?>
 
-<!-- GSAP -->
-<script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/ScrollTrigger.min.js"></script>
+	<!-- GSAP -->
+	<script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/ScrollTrigger.min.js"></script>
 
-<!-- AOS -->
-<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+	<!-- AOS -->
+	<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+	<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 
 </head>
+
 <body <?php body_class(); ?>>
-	<?php 
-	if ( function_exists( 'wp_body_open' ) ) {
-
+	<?php
+	if (function_exists('wp_body_open')) {
 		wp_body_open();
-
 	} ?>
-<div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content','edge');?></a>
-<!-- Masthead ============================================= -->
-<header id="masthead" class="site-header" role="banner">
-	<div class='main-tool-bar'></div>
-	
-		<?php if ( function_exists( 'the_custom_header_markup' ) ) {
-			if ( is_header_video_active() && ( has_header_video() || is_customize_preview() ) ) {
-				echo '<div class="custom-header">
+	<div id="page" class="hfeed site">
+		<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'edge'); ?></a>
+		<!-- Masthead ============================================= -->
+		<header id="masthead" class="site-header" role="banner">
+			<div class='main-tool-bar'></div>
+
+			<?php if (function_exists('the_custom_header_markup')) {
+				if (is_header_video_active() && (has_header_video() || is_customize_preview())) {
+					echo '<div class="custom-header">
 					<div class="custom-header-media">';
-				the_custom_header_markup();
-				echo '</div>
+					the_custom_header_markup();
+					echo '</div>
 				</div>';
-			}else{ 
-				if ( has_header_image() ) {?>
-					<a href="<?php echo esc_url(home_url('/'));?>"><img src="<?php header_image(); ?>" class="header-image" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="<?php echo esc_attr(get_bloginfo('name', 'display'));?>" /> </a>
+				} else {
+					if (has_header_image()) { ?>
+						<a href="<?php echo esc_url(home_url('/')); ?>"><img src="<?php header_image(); ?>" class="header-image" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" /> </a>
 				<?php }
-			}
-		} else { ?>
-		<a href="<?php echo esc_url(home_url('/'));?>"><img src="<?php header_image(); ?>" class="header-image" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="<?php echo esc_attr(get_bloginfo('name', 'display'));?>" /> </a>
-		<?php } ?>
-		<!-- <div class="top-header"> -->
+				}
+			} else { ?>
+				<a href="<?php echo esc_url(home_url('/')); ?>"><img src="<?php header_image(); ?>" class="header-image" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" /> </a>
+			<?php } ?>
+			<!-- <div class="top-header"> -->
 			<!-- <div class="container clearfix"> -->
-				<?php
-				// if( is_active_sidebar( 'edge_header_info' )) {
-				// 	dynamic_sidebar( 'edge_header_info' );
-				// }
-				// if($edge_settings['edge_top_social_icons'] == 0):
-				// 	echo '<div class="header-social-block">';
-				// 		do_action('social_links');
-				// 	echo '</div>'.'<!-- end .header-social-block -->';
-				// endif;
-				// do_action('edge_site_branding'); ?>
+			<?php
+			// if( is_active_sidebar( 'edge_header_info' )) {
+			// 	dynamic_sidebar( 'edge_header_info' );
+			// }
+			// if($edge_settings['edge_top_social_icons'] == 0):
+			// 	echo '<div class="header-social-block">';
+			// 		do_action('social_links');
+			// 	echo '</div>'.'<!-- end .header-social-block -->';
+			// endif;
+			// do_action('edge_site_branding'); 
+			?>
 			<!-- </div> -->
-			 <!-- end .container -->
-		<!-- </div>  -->
-		<!-- end .top-header -->
+			<!-- end .container -->
+			<!-- </div>  -->
+			<!-- end .top-header -->
 
 
-		<!-- Main Header============================================= -->
-				<!-- <div id="sticky_header"> -->
-				<div id="">
+			<!-- Main Header============================================= -->
+			<!-- <div id="sticky_header"> -->
+			<div id="">
 
-					<div class="header-content container clearfix">
+				<div class="header-content container clearfix">
 					<!-- LOGO  -->
-						<?php do_action('edge_site_branding'); ?>
+					<?php do_action('edge_site_branding'); ?>
 
 					<!-- Main Nav ============================================= -->
-						<?php
-							if (has_nav_menu('primary')) { ?>
+					<?php
+					if (has_nav_menu('primary')) { ?>
 						<?php $args = array(
 							'theme_location' => 'primary',
 							'container'      => '',
 							'items_wrap'     => '<ul id="primary-menu" class="menu nav-menu">%3$s</ul>',
-							); ?>
-						<nav id="site-navigation" class="main-navigation clearfix" role="navigation" aria-label="<?php esc_attr_e('Main Menu','edge');?>">
+						); ?>
+						<nav id="site-navigation" class="main-navigation clearfix" role="navigation" aria-label="<?php esc_attr_e('Main Menu', 'edge'); ?>">
 							<!-- <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false" type="button">
 								<span class="line-one"></span>
 					  			<span class="line-two"></span>
 					  			<span class="line-three"></span>
 						  	</button> -->
-					  		<!-- end .menu-toggle -->
-							<?php wp_nav_menu($args);//extract the content from apperance-> nav menu ?>
+							<!-- end .menu-toggle -->
+							<?php wp_nav_menu($args); //extract the content from apperance-> nav menu 
+							?>
 						</nav> <!-- end #site-navigation -->
-						<?php } // extract the content from page menu only ?>
-						<!-- <nav id="site-navigation" class="main-navigation clearfix" role="navigation" aria-label="<?php //esc_attr_e('Main Menu','edge');?>"> -->
-							<!-- <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false" type="button">
+					<?php } // extract the content from page menu only 
+					?>
+					<!-- <nav id="site-navigation" class="main-navigation clearfix" role="navigation" aria-label="<?php //esc_attr_e('Main Menu','edge');
+																													?>"> -->
+					<!-- <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false" type="button">
 								<span class="line-one"></span>
 					  			<span class="line-two"></span>
 					  			<span class="line-three"></span>
 						  	</button> -->
-					  	<!-- end .menu-toggle -->
-							<?php	// wp_page_menu(array('menu_class' => 'menu', 'items_wrap'     => '<ul id="primary-menu" class="menu nav-menu">%3$s</ul>')); ?>
-						<!-- </nav> -->
-						 <!-- end #site-navigation -->
-						
-						<div class="login">login logout</div>
-			</div> <!-- end .container -->
-			</div> <!-- end #sticky_header -->
-			<?php 
-		// $enable_slider = $edge_settings['edge_enable_slider'];
-		// edge_slider_value();
-		// if ($enable_slider=='frontpage'|| $enable_slider=='enitresite'){
-		// 	if(is_front_page() && ($enable_slider=='frontpage') ) {
-		// 		if($edge_settings['edge_slider_type'] == 'default_slider') {
-		// 				edge_page_sliders();
-		// 		}else{
-		// 			if(class_exists('Edge_Plus_Features')):
-		// 				edge_image_sliders();
-		// 			endif;
-		// 		}
-		// 	}
-		// 	if($enable_slider=='enitresite'){
-		// 		if($edge_settings['edge_slider_type'] == 'default_slider') {
-		// 				edge_page_sliders();
-		// 		}else{
-		// 			if(class_exists('Edge_Plus_Features')):
-		// 				edge_image_sliders();
-		// 			endif;
-		// 		}
-		// 	}
-		// } ?>
-</header> <!-- end #masthead -->
-<!-- Main Page Start ============================================= -->
-<div id="content">
-<div class="container clearfix">
+					<!-- end .menu-toggle -->
+					<?php	// wp_page_menu(array('menu_class' => 'menu', 'items_wrap'     => '<ul id="primary-menu" class="menu nav-menu">%3$s</ul>')); 
+					?>
+					<!-- </nav> -->
+					<!-- end #site-navigation -->
 
+					<button class="btn btn--solid login--grey">login</button>
+				</div> <!-- end .container -->
+			</div> <!-- end #sticky_header -->
+			<?php
+			// $enable_slider = $edge_settings['edge_enable_slider'];
+			// edge_slider_value();
+			// if ($enable_slider=='frontpage'|| $enable_slider=='enitresite'){
+			// 	if(is_front_page() && ($enable_slider=='frontpage') ) {
+			// 		if($edge_settings['edge_slider_type'] == 'default_slider') {
+			// 				edge_page_sliders();
+			// 		}else{
+			// 			if(class_exists('Edge_Plus_Features')):
+			// 				edge_image_sliders();
+			// 			endif;
+			// 		}
+			// 	}
+			// 	if($enable_slider=='enitresite'){
+			// 		if($edge_settings['edge_slider_type'] == 'default_slider') {
+			// 				edge_page_sliders();
+			// 		}else{
+			// 			if(class_exists('Edge_Plus_Features')):
+			// 				edge_image_sliders();
+			// 			endif;
+			// 		}
+			// 	}
+			// } 
+			?>
+		</header> <!-- end #masthead -->
+		<!-- Main Page Start ============================================= -->
+		<div id="content">
+			<div class="container clearfix">
