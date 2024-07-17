e58d6b37a5af7fd17d3b2ce7692ade3b7c74d3fc
Michael <mw910114@gmail.com>
2024-07-17 15:24:48 +0100

    change aos offset

--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -144,7 +144,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // AOS
   AOS.init({
     // disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-    // offset: 120, // offset (in px) from the original trigger point
+    offset: 0, // offset (in px) from the original trigger point
     // delay: 0, // values from 0 to 3000, with step 50ms
     duration: 600, // values from 0 to 3000, with step 50ms
     easing: 'ease-out', // default easing for AOS animations
