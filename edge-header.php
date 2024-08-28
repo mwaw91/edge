757274b898e9d37bb87e085472871b1738c2e746
Michael <mw910114@gmail.com>
2024-08-28 15:19:30 +0100

    increment styles

--- edge-header.php
+++ edge-header.php
@@ -45,7 +45,7 @@
 
 
     <!-- CSS -->
-    <link rel="stylesheet" type="text/css" href="https://skybox.gg/wp-content/themes/hello-elementor-child/edge-page/child-theme-85.css">
+    <link rel="stylesheet" type="text/css" href="https://skybox.gg/wp-content/themes/hello-elementor-child/edge-page/child-theme-86.css">
 
 </head>
 
